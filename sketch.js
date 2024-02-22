906a71a57bceffae65f7ffd350f4e2fe13c343b4
Caleb Wright <wrightcl2@appstate.edu>
2024-02-21 23:44:42 -0500

    Works but there is a bug where when you hit the left bound of the screen everything crashes

--- sketch.js
+++ sketch.js
@@ -1,56 +1,100 @@
 function make2Darray(cols, rows) {
-    let arr = new Array(cols);
-    for (let i = 0; i < arr.length; i ++) {
-        arr[i] = new Array(rows);
-        for(let j = 0; j < arr[i].length; j ++)
-        arr[i][j] = 0;
-    }
-    return arr;
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+    for (let j = 0; j < arr[i].length; j++) arr[i][j] = 0;
+  }
+  return arr;
 }
 
-let grid; 
-let w = 10;
+let grid;
+let w = 5;
 let cols, rows;
+let hueValue = 200;
 
-function setup(){
-    createCanvas(400, 400);
-    cols = width / w;
-    rows = height / w;
-    grid = make2Darray(cols, rows);
-
-    for (let i = 0; i < cols; i ++){
-        for(let j = 0; j < rows; j++){
-            grid[i][j] = 0;
-        }
-    }
-    grid[20][10] = 1
+function withinCols(i) {
+  return i >= 0 && i <= cols - 1;
 }
 
-function draw(){
-    background(220);
-    for (let i = 0; i < cols; i ++){
-        for(let j = 0; j < rows; j++){
-            stroke(255)
-            fill(grid[i][j]*255);
-            let x = i * w;
-            let y = j * w;
-            square(x, y, w)
-        }
-    }
-
-
-let nextGrid = make2Darray(cols, rows);
-for (let i = 0; i < cols; i ++){
-    for(let j = 0; j < rows; j++){
-        let state = grid[i][j];
-        if (state === 1){
-            let below = grid[i][j+1];
-            if (below === 0) {
-                nextGrid[i][j] = 0;
-                nextGrid[i][j+1] = 1;
-            }
-        }
-    }
-    grid = nextGrid;
+function withinRows(j) {
+  return j >= 0 && j <= rows - 1;
 }
+
+function setup() {
+  createCanvas(1900, 1000);
+  colorMode(HSB, 360, 255, 255);
+  cols = width / w;
+  rows = height / w;
+  grid = make2Darray(cols, rows);
+}
+
+function mouseDragged() {
+  let mouseCol = floor(mouseX / w);
+  let mouseRow = floor(mouseY / w);
+
+  let matrix = 3;
+  let extent = floor(matrix / 2);
+  for (let i = -extent; i <= extent; i++) {
+    for (let j = -extent; j <= extent; j++) {
+      if (random(1) < 0.5) {
+        let col = mouseCol + i;
+        let row = mouseRow + j;
+        if (withinCols(col) && withinRows(row)) {
+          grid[col][row] = hueValue;
+        }
+      }
+    }
+  }
+  hueValue += 0.5;
+  if (hueValue > 360) {
+    hueValue = 1;
+  }
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      noStroke();
+      if (grid[i][j] > 0) {
+        fill(grid[i][j], 255, 255);
+        let x = i * w;
+        let y = j * w;
+        square(x, y, w);
+      }
+    }
+  }
+
+  let nextGrid = make2Darray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      if (state > 1) {
+        let below = grid[i][j + 1];
+
+        let dir = random([-1, 1]);
+
+        let belowA = -1;
+        let belowB = -1;
+
+        if (withinCols(i + dir)) {
+          belowA = grid[i + dir][j + 1];
+        }
+        if (withinRows(i + dir)) {
+          belowB = grid[i - dir][j + 1];
+        }
+
+        if (below === 0) {
+          nextGrid[i][j + 1] = state;
+        } else if (belowA === 0) {
+          nextGrid[i + dir][j + 1] = state;
+        } else if (belowB === 0) {
+          nextGrid[i - dir][j + 1] = state;
+        } else {
+          nextGrid[i][j] = state;
+        }
+      }
+    }
+  }
+  grid = nextGrid;
 }
